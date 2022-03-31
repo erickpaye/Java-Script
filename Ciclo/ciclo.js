f145b2aa4b6f26e261ab5ff3334b12ff70d4a3c7
Erick <erickpayelucas@gmail.com>
2022-03-30 22:37:21 -0300

    Desafio complementario

---
+++ Ciclo/ciclo.js
@@ -0,0 +1,28 @@
+let entrada = prompt("Ingrese su Selecion participante de las eliminatorias");
+while(entrada != "Me Aburri" ){
+    switch(entrada){
+        case "Brasil":
+            alert("Bienvenido al Mundial");
+            break;
+        case "Argentina":
+            alert("Bienvenido al Mundial");
+            break;
+        case "Ecuador":
+            alert("Bienvenido al Mundial");
+            break;
+        case "Uruguay":
+            alert("Bienvenido al Mundial");
+            break;
+        case "Peru":
+            alert("Vos todavia no");
+            break;            
+        case "Chile":
+            alert("Chi Chi Chi Led Led Led");
+            break;    
+        default:
+            alert("Sali de aca wacho");
+            break;
+
+    }
+    entrada = prompt ("Ingrese Seleccion participante de las eliminatorias")
+}
---
+++ java/cap.js
@@ -0,0 +1,9 @@
+let Sexo = prompt(" Ingrese su Sexo")
+
+if (Sexo == "mujer"){
+    alert ("Precio de tu entrada $0")
+} else {
+    alert("vos paga $5000")
+}
+
+
